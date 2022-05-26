53dc736265b951248cba3cbfd23ef1717a55e7be
Vikrant <vikrant2812@gmail.com>
2022-05-27 03:32:28 +0530

    fix: style

--- src/Components/CreatePost.jsx
+++ src/Components/CreatePost.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CreatePost = () => {
   return (
     <div className="border-2 rounded-md p-4">
-        <textarea placeholder='Write Something Intresting...' className='border-none focus:outline-none w-full'/>
+        <textarea placeholder='Write Something Intresting...' className='border-none focus:outline-none w-full resize-none '/>
         <div className="flex justify-between">
         <p>😀</p>
         <button className='w-[80px] py-1 rounded-2xl text-black-700 bg-blue-400 hover:bg-blue-500  font-medium '> Post</button>
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -3,9 +3,9 @@ import { FcSearch } from "react-icons/fc";
 const Search = () => {
   return (
     <div className="flex items-center mt-5 w-full md:w-full h-10">
-        <FcSearch className='absolute top-22 left-19 z-10 mr-2'/>
+        <FcSearch className='absolute top-22 left-19 z-10 mr-2 ml-2'/>
         <input type="text" placeholder=' Search'
-        className="relative border w-full h-5 px-3 py-5 mt-2 mb-2 rounded-md focus:ring-1 focus:outline-none focus:ring-blue-500"
+        className="relative border w-full h-5 p-5 mt-2 mb-2 rounded-md focus:ring-1 focus:outline-none focus:ring-blue-500"
         />
     </div>
   )
